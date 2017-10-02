80d900fb4ff34b82056637a699c9904aea9f18be
Ismail GACHA <igacha664@gmail.com>
2017-10-02 11:19:54 +0200

    fin exo01

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -17,7 +17,14 @@ public class Method_01_Test {
     interface IDao {
         List<Person> findAll();
 
-        // TODO créer une méthode int sumAge()
+         // TODO créer une méthode int sumAge()
+        default int sumAge(){
+        	int sum = 0;
+        	for (Person p : findAll()){
+        		sum += p.getAge();
+        	}
+        	return sum;
+        }
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
     }
     // end::IDao[]
@@ -48,7 +55,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +66,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
